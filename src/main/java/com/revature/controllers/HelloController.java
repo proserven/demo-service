b0f144b5247e8f81d00ed982bff9ae216981332f
proserven <proserven@users.noreply.github.com>
2022-06-18 17:19:46 -0400

    Lastest update

--- src/main/java/com/revature/controllers/HelloController.java
+++ src/main/java/com/revature/controllers/HelloController.java
@@ -11,17 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	static final String SERVER_PORT = "local.server.port";
+
 	@Autowired
 	private Environment env;
 
 	//localhost:8080/hello
 	@GetMapping("/hello")
 	public String sayHello() throws UnknownHostException {
-		return String.format("Hello from %s:%s", InetAddress.getLocalHost().getHostAddress(), env.getProperty("local.server.port"));
+		return String.format("Hello from %s:%s", InetAddress.getLocalHost().getHostAddress(), env.getProperty(SERVER_PORT));
 	}
 
 	@GetMapping("/hi")
 	public String sayHi() throws UnknownHostException {
-		return String.format("Hello Luis looks for the info:  %s:%s", InetAddress.getLocalHost().getHostAddress(), env.getProperty("local.server.port"));
+		return String.format("Hello Luis looks for the info:  %s:%s", InetAddress.getLocalHost().getHostAddress(), env.getProperty(SERVER_PORT));
+	}
+
+	@GetMapping("/howdy")
+	public String howdy() throws UnknownHostException {
+		return String.format("Hello Luis looks for the info:  %s:%s", InetAddress.getLocalHost().getHostAddress(), env.getProperty(SERVER_PORT));
 	}
 }
